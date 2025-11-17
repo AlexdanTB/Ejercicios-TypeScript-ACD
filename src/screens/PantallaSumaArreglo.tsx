e7e473fafc48af414b9558fb6eeaa1b38c16a38a
AlexdanTB <alexdanieltb220@gmail.com>
2025-11-17 17:58:34 -0500

    Calcular la suma de numeros en arreglo

--- src/screens/PantallaSumaArreglo.tsx
+++ src/screens/PantallaSumaArreglo.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { styles } from '../theme/appStyle'
 
 export const PantallaSumaArreglo = () => {
+    const nums= [5, 6, 8, 2, 3, 11]
+    const [suma, setsuma] = useState<number>()
+
+    const sumar = ()=>{
+        nums.reduce((a: number, na: number)=>{
+            setsuma(a+na)
+            return a + na;
+        }, 0);
+    }
   return (
-    <View>
-        <Text>Suma Arreglo</Text>
+    <View style={styles.vw2}>
+        <Text style={styles.titulo}>Suma arreglo de números</Text>
+        <Text>{nums.join(", ")}</Text>
+        <TouchableOpacity onPress={sumar} style={styles.btns}>
+            <Text>Sumar</Text>
+        </TouchableOpacity>
+        <Text>Resultado = {suma}</Text>
+
     </View>
   )
 }
