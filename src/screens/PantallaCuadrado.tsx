4404de8ae721a2a528e12b8eb4c039967db3dd6e
AlexdanTB <alexdanieltb220@gmail.com>
2025-11-17 14:13:09 -0500

    Creación de tema (estilos)

--- src/screens/PantallaCuadrado.tsx
+++ src/screens/PantallaCuadrado.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { styles } from '../theme/appStyle'
 
 export const PantallaCuadrado = () => {
     return (
         <View>
-            <Text>Cuadrado</Text>
+            <Text style={styles.titulo}>Calcular área</Text>
         </View>
     )
 }
---
+++ src/theme/appStyle.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    titulo:{
+        fontSize:18,
+        fontWeight:"bold",
+        textAlign:"center"
+    }
+})
