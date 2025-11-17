dc983c7769235ad71a7eb65a817683402641f36f
AlexdanTB <alexdanieltb220@gmail.com>
2025-11-17 12:25:38 -0500

    Creación de pantallas

---
+++ src/screens/PantallaDivisionArreglo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const PantallaDivisionArreglo = () => {
+  return (
+    <View>
+        <Text>División Arreglo</Text>
+    </View>
+  )
+}
