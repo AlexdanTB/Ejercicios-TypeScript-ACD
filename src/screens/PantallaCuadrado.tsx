b447508d7940e02cedabedb74424da0d357a5ece
AlexdanTB <alexdanieltb220@gmail.com>
2025-11-17 15:10:06 -0500

    Diseño UI  de PantallaCuadrado

--- src/screens/PantallaCuadrado.tsx
+++ src/screens/PantallaCuadrado.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { styles } from '../theme/appStyle'
 
 export const PantallaCuadrado = () => {
     return (
-        <View>
+        <View style={styles.vw2}>
             <Text style={styles.titulo}>Calcular área</Text>
+            <Text>Ingresa el lado del cuadrado</Text>
+            <TextInput style={styles.input} placeholder='cm'></TextInput>
+            <TouchableOpacity style={styles.btns}>
+                <Text>Calcular</Text>
+            </TouchableOpacity>
+            <Text>El área del cuadrado es: </Text>
         </View>
     )
 }
--- src/theme/appStyle.ts
+++ src/theme/appStyle.ts
@@ -1,9 +1,29 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+    vw2:{
+        gap:10,
+        marginVertical:10,
+        alignItems:"center"
+    },
     titulo:{
         fontSize:18,
         fontWeight:"bold",
-        textAlign:"center"
+        textAlign:"center",
+        margin:15
+    },
+    input:{
+        backgroundColor: "#fdfdfdff",
+        padding:5,
+        width:"50%",
+        borderColor:"#b581adff",
+        borderWidth:0.5,
+        margin:5
+    },
+    btns:{
+        backgroundColor:"#f4d1c9ff",
+        padding:10,
+        paddingHorizontal:20,
+        borderRadius:5
     }
 })
