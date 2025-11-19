e537bd9a2e403d4837c90b358cbc9a3a4f9345b1
AlexdanTB <alexdanieltb220@gmail.com>
2025-11-19 11:44:32 -0500

    Creación arreglo de objetos Alumno

--- src/screens/PantallaPromedio.tsx
+++ src/screens/PantallaPromedio.tsx
@@ -3,15 +3,21 @@ import { Text, View } from 'react-native'
 
 export const PantallaPromedio = () => {
 
-  interface Alumno{
+  interface Alumno {
     nombre: string;
     edad: number;
     calificacion: number;
   }
-  
+
+  const alumnos: Alumno[] = [
+    {nombre: "Viviana", edad: 19, calificacion: 10},
+    {nombre: "Wendy", edad: 20, calificacion: 8},
+    {nombre: "Gerson", edad: 18, calificacion: 9}
+  ]
+
   return (
     <View>
-        <Text>Promedio</Text>
+      <Text>Promedio</Text>
     </View>
   )
 }
