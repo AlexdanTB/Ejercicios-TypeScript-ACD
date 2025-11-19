e76a12f08be15e84af902be2e2ad3806bde40171
AlexdanTB <alexdanieltb220@gmail.com>
2025-11-19 11:37:14 -0500

    Definición de interfaz Alumno

--- src/screens/PantallaPromedio.tsx
+++ src/screens/PantallaPromedio.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { Text, View } from 'react-native'
 
 export const PantallaPromedio = () => {
+
+  interface Alumno{
+    nombre: string;
+    edad: number;
+    calificacion: number;
+  }
+  
   return (
     <View>
         <Text>Promedio</Text>
